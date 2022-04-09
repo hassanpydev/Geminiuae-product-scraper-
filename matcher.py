db7d50a7b96a4ae13067931c941cd6091cc055bb
hassanpydev <hassangames1998@gmail.com>
2022-04-09 11:46:52 +0300

    fuxxxy
    adadawd

--- matcher.py
+++ matcher.py
@@ -58,7 +58,7 @@ for category in readCategory():
     print(category)
     best_match = findMatch(category)
     if best_match[1] > 80:
-        updateCategory_By_name(categories_id(best_match[0]))
+        updateCategory_By_name(categories_id.get(best_match[0]))
         print(best_match)
     else:
         print("No match found")
